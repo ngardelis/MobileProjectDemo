a21af6b8950273e3926d7d55262b628124dd5192
ngardelis <118944371+ngardelis@users.noreply.github.com>
2023-10-03 13:13:16 +0300

    Initial Commit

---
+++ MobileProjectDemo/MobileProjectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileProjectDemoApp.swift
+//  MobileProjectDemo
+//
+//  Created by nikos gardelis on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MobileProjectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
